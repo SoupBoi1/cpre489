e4457994a5d4ae70bd83c237fc9b69a3350dad2d
sudipta <sudipta@co2061-20.ece.iastate.edu>
2024-09-19 15:32:12 -0500

    bind not failing or work?

--- lab2/sever.c
+++ lab2/sever.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-struct sockaddr_in
-{
-    int sin_family;
-    int sin_port;
-    long s_arrd; // todo
-};
-struct in_addr
-{
-    unsigned long s_addr; 
-};
 
 
 
 int main(){
 
     
-    int ok = socket(PF_INET,SOCK_STREAM,0);
-
+    int socket_Result = socket(PF_INET,SOCK_STREAM,0);
+    //port 17
+   // int port = htops(17);
+    //int ip = inet_addr("8.8.8.8");
+   // struct  sockaddr sockAddr ={PF_INET,htops(17),inet_addr("8.8.8.8")};
+    //int bind_v = bind(socket_Result,&sockAddr,sizeof(sockAddr) );
+int bind_v =0;
     // sever
  //   struct sockaddr_in addr = {PF_INET,55,(long)77}; 
 //    htons(int port);
 //    inet_addr();
  //   inet_addr(char *) 
 
-    printf("%d",&ok);
+    printf("%d\n%d\n",&socket_Result,&bind_v);
     return 0;
 }
 
