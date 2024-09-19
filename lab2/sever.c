e0899790206255453244bdaac9069697fe0db35f
sudipta <sudipta@co2061-20.ece.iastate.edu>
2024-09-19 15:03:26 -0500

    init file creation

---
+++ lab2/sever.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/socket.h>
+
+struct sockaddr_in
+{
+    int sin_family;
+    int sin_port;
+    long s_arrd; // todo
+};
+struct in_addr
+{
+    unsigned long s_addr; 
+};
+
+
+
+int main(){
+
+    
+    int ok = socket(PF_INET,SOCK_STREAM,0);
+
+    // sever
+ //   struct sockaddr_in addr = {PF_INET,55,(long)77}; 
+//    htons(int port);
+//    inet_addr();
+ //   inet_addr(char *) 
+
+    printf("%d",&ok);
+    return 0;
+}
+
