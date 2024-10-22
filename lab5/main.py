1cbe24ca25deca5ae4b1b84cc65daf1d00d54cfd
Sudipta Halder <66485000+SoupBoi1@users.noreply.github.com>
2024-10-21 20:22:46 -0500

    lab5 recouded data

---
+++ lab5/main.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+t0 =[]
+t2 =[]
+t1 =[]
+p0 =[]
+p1=[]
+p2=[]
+
+with open("tcpprobe.dat", "r") as file:
+    for line in file:
+
+        if(line.split()[1]=="10.10.1.2:51110"):
+            t0.append(float(line.split()[0]));
+            p0.append(float(line.split()[6]));
+            print(line.split()[0],line.split()[1],line.split()[6])
+
+        if(line.split()[1]=="10.10.1.2:51112"):
+            t1.append(float(line.split()[0]));
+            p1.append(float(line.split()[6]));
+            print(line.split()[0],line.split()[1],line.split()[6])
+        if(line.split()[1]=="10.10.1.2:51114"):
+            t2.append(float(line.split()[0]));
+            p2.append(float(line.split()[6]));
+            print(line.split()[0],line.split()[1],line.split()[6])
+
+
+
+
+
+
+# evenly sampled time at 200ms intervals
+
+# red dashes, blue squares and green triangles
+plt.figure()
+plt.subplot(311)
+plt.plot(t0,p0)
+
+plt.subplot(312)
+plt.plot(t1,p1)
+
+plt.subplot(313)
+plt.plot(t2,p2)
+
+
+
+plt.show()
+f.close
