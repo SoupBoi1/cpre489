cff7ddb34c16a35c7aa0ffbeb681f85d0c228347
Sudipta Halder <66485000+SoupBoi1@users.noreply.github.com>
2024-10-21 21:35:59 -0500

    lab5 recouded data

--- lab5/main.py
+++ lab5/main.py
@@ -6,6 +6,9 @@ t1 =[]
 p0 =[]
 p1=[]
 p2=[]
+h0=[]
+h1=[]
+h2=[]
 
 with open("tcpprobe.dat", "r") as file:
     for line in file:
@@ -13,15 +16,20 @@ with open("tcpprobe.dat", "r") as file:
         if(line.split()[1]=="10.10.1.2:51110"):
             t0.append(float(line.split()[0]));
             p0.append(float(line.split()[6]));
+            h0.append(float(line.split()[7]));
             print(line.split()[0],line.split()[1],line.split()[6])
 
         if(line.split()[1]=="10.10.1.2:51112"):
             t1.append(float(line.split()[0]));
             p1.append(float(line.split()[6]));
+            h1.append(float(line.split()[7]));
+
             print(line.split()[0],line.split()[1],line.split()[6])
         if(line.split()[1]=="10.10.1.2:51114"):
             t2.append(float(line.split()[0]));
             p2.append(float(line.split()[6]));
+            h2.append(float(line.split()[7]));
+
             print(line.split()[0],line.split()[1],line.split()[6])
 
 
@@ -35,12 +43,16 @@ with open("tcpprobe.dat", "r") as file:
 plt.figure()
 plt.subplot(311)
 plt.plot(t0,p0)
+plt.plot(t0,h0)
 
 plt.subplot(312)
 plt.plot(t1,p1)
+plt.plot(t1,h1)
+
 
 plt.subplot(313)
 plt.plot(t2,p2)
+plt.plot(t2,h2)
 
 
 
