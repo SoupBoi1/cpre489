f4e4af18eff07421497cb7d06f9a21a46e1c6da8
Sudipta Halder <66485000+SoupBoi1@users.noreply.github.com>
2024-10-28 14:32:16 -0500

    python script

--- lab5/main.py
+++ lab5/main.py
@@ -4,33 +4,33 @@ t0 =[]
 t2 =[]
 t1 =[]
 p0 =[]
-p1=[]
+p1=[] #cwnd
 p2=[]
 h0=[]
 h1=[]
-h2=[]
+h2=[] #ssthresh
 
 with open("tcpprobe.dat", "r") as file:
     for line in file:
 
         if(line.split()[1]=="10.10.1.2:51110"):
-            t0.append(float(line.split()[0]));
-            p0.append(float(line.split()[6]));
-            h0.append(float(line.split()[7]));
-            print(line.split()[0],line.split()[1],line.split()[6])
+            t0.append(float(line.split()[0]))
+            p0.append(float(line.split()[6]))
+            h0.append(float(line.split()[7]))
+            #print(line.split()[0],line.split()[1],line.split()[6])
 
         if(line.split()[1]=="10.10.1.2:51112"):
-            t1.append(float(line.split()[0]));
-            p1.append(float(line.split()[6]));
-            h1.append(float(line.split()[7]));
+            t1.append(float(line.split()[0]))
+            p1.append(float(line.split()[6]))
+            h1.append(float(line.split()[7]))
 
-            print(line.split()[0],line.split()[1],line.split()[6])
+         #   print(line.split()[0],line.split()[1],line.split()[6])
         if(line.split()[1]=="10.10.1.2:51114"):
-            t2.append(float(line.split()[0]));
-            p2.append(float(line.split()[6]));
-            h2.append(float(line.split()[7]));
+            t2.append(float(line.split()[0]))
+            p2.append(float(line.split()[6]))
+            h2.append(float(line.split()[7])) #ssthresh
 
-            print(line.split()[0],line.split()[1],line.split()[6])
+        print(line.split())
 
 
 
@@ -41,18 +41,29 @@ with open("tcpprobe.dat", "r") as file:
 
 # red dashes, blue squares and green triangles
 plt.figure()
+plt.xlabel("time (s)")
+plt.ylabel("CWND (in MSS)")
+
 plt.subplot(311)
-plt.plot(t0,p0)
-plt.plot(t0,h0)
+plt.plot(t0,p0,"r")
+plt.plot(t0,h0,"b--")
+
+plt.title("10.10.1.2:51110")
+
 
 plt.subplot(312)
-plt.plot(t1,p1)
-plt.plot(t1,h1)
+plt.plot(t1,p1,"r")
+plt.plot(t1,h1,"b--")
+plt.title("10.10.1.2:51112")
+
 
 
 plt.subplot(313)
-plt.plot(t2,p2)
-plt.plot(t2,h2)
+plt.plot(t2,p2,"r")
+plt.plot(t2,h2,"b--")
+plt.title("10.10.1.2:51114")
+
+plt.legend(['cwnd','ssthresh'],bbox_to_anchor = (1,-.15),ncol=2) 
 
 
 
