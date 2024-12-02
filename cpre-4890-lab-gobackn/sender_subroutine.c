6e6a8da6b8284e4bb227b97c839e951ba8272f7b
sudipta <sudipta@linuxvdi-40.ece.iastate.edu>
2024-12-02 13:51:16 -0600

    commented and done

--- cpre-4890-lab-gobackn/sender_subroutine.c
+++ cpre-4890-lab-gobackn/sender_subroutine.c
@@ -12,12 +12,11 @@
  * ber 	  - bit error rate which must be passed to IntroduceError */
 void primary(int sockfd, double ber) {
 
-    /* TODO: Replace this function with your solution according to the lab
-     * manual. */
 
 
     int read_size=3;
-    int window =3;
+    int window =3; // Go-Back-N ARQ protocol with a send window of size N = 3
+
     int buffer_window=0; // our send window 
     int send_meg_size =26; // size of the message in  number of char
     char send_msg[26]="ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // our message 
@@ -38,18 +37,15 @@ void primary(int sockfd, double ber) {
        // printf("%i %c%c \n",i,send_msg[0+i*2],send_msg[1+i*2]); 
 
         build_packet(&packetArray[i],PKT_TYPE_DATA,temPacketdata,i); // creating packet 
-        print_packet(&packetArray[i]);
+        //print_packet(&packetArray[i]); 
 
     }
     
-    
-
     printf("---------Beginning subroutine---------\n");
-    
-    int stop_reciving = 0;
 
     while (buffer_window < number_of_packets ){ // does 
-        printf("\n\nBUFFER INDEX=: %d \nPorgrss:%d/%d\n ",buffer_window,number_of_packets-buffer_window ,number_of_packets);
+
+        printf("\n\nSEND WINDOW := %d \nPROGRESS: %d/%d\n",buffer_window,number_of_packets-buffer_window ,number_of_packets); // start of sending windsize packects
 
         for(int i =0; i< window;i++){// send the packets in the window 
             if (buffer_window+i< number_of_packets){ // checks if the buffer window isn't over the limt of the array
@@ -59,39 +55,39 @@ void primary(int sockfd, double ber) {
                     perror("Send failed");
 
                 printf("Sent packet: ");
-                print_packet(&packetArray[buffer_window +i]);
-                print_packet(&tem_packet_with_errors);
-                stop_reciving = 0;
+                print_packet(&packetArray[buffer_window +i]); // prints the packet send
+
             }
         }
+
         tem_send_window =buffer_window; //used to hold the buffer_window value before being updtae in reciving ack and nack
-        for(int i =0; i< window;i++){ 
-            if (tem_send_window+i< number_of_packets){ 
+        for(int i =0; i< window;i++){  // recives all the packect sent
+            if (tem_send_window+i< number_of_packets){  // // checks if the buffer window isn't over the limt of the array
                 if ((read_size = recv(sockfd, srv_reply, PKT_SIZE, 0)) < 0) { // reciveinng the packect look for the three packects 
                     perror("recv failed");
                 } else {
-                    printf("\n%d\n",i);
-                    printf("Received packet: ");
+                    printf("%d.Received packet: ",i+1); // prints the start of the recive packects
                     print_packet((packet_t *)&srv_reply); //prints the packet recived
                     tem_number_of_reply = sizeof((packet_t *)&srv_reply)/sizeof((packet_t *)&srv_reply)[0]; // size of the arrqy of the number of packets recived at that movement
                     
                     for (int y =0; y< tem_number_of_reply;y++){ // goes through all the packect recived
 
                         if( ((packet_t *)srv_reply)[y].type == PKT_TYPE_ACK ){ // checks if it's a ack type apck
-                            printf("got ACK: looking for %d\n",buffer_window+1);
+                            printf("\tGOT ACK: looking for %d\n",buffer_window+1); // print to indicate it's an ACK
                             if (buffer_window == (((packet_t *)srv_reply)[y].sequence_number)-1 ){ // if its the correct sequence number 
-                                printf("%d+1 == %d\n",buffer_window,(((packet_t *)srv_reply)[y].sequence_number));
                                 buffer_window+=1; // adjectig the window size
+                                printf("\tgot %d, update window size to %d \n",(((packet_t *)srv_reply)[y].sequence_number),buffer_window); // print ditails
+
                             }else{
-                                printf(" NOT %d != %d -1\n",buffer_window,(((packet_t *)srv_reply)[y].sequence_number));
+                                printf("\tgot %d IGNORE\n",(((packet_t *)srv_reply)[y].sequence_number)); // prints if duplicate and some other reaso for not working 
 
                             }
 
                         }
 
-                        if( ((packet_t *)srv_reply)[y].type == PKT_TYPE_NAK ){ // checks if it's a Nack type apck
-                            printf("got NCK: current sendwindow %d go to %d \n",buffer_window,((packet_t *)srv_reply)[y].sequence_number -window);
-                            buffer_window = ((packet_t *)srv_reply)[y].sequence_number ;
+                        if( ((packet_t *)srv_reply)[y].type == PKT_TYPE_NAK ){ // checks if it's a Nack 
+                            printf("\tGOT NACK: current sendwindow %d go to %d \n",buffer_window,((packet_t *)srv_reply)[y].sequence_number -window); // print to indicate it's an ACK
+                            buffer_window = ((packet_t *)srv_reply)[y].sequence_number ; // set the window size to apropiact index to send the NACK
                         }
                     }
 
@@ -104,32 +100,4 @@ void primary(int sockfd, double ber) {
 
     }
 
-
-/*
-    if ((read_size = recv(sockfd, srv_reply, PKT_SIZE, 0)) < 0) {
-        perror("recv failed");
-    } else {
-        printf("Received packet: ");
-        print_packet((packet_t *)&srv_reply);
-    }
-
-    if (send(sockfd, &packetArray[1], sizeof(packetArray[1]), 0) < 0)
-        perror("Send failed");
-    printf("Sent packet: ");
-    print_packet(&packetArray[1]);
-*/
-    /* Receive a reply from the server
-     * Note:
-     *   Sockets, and recv, do not keep messages separated. For example, If you
-     *   receive two 16-byte messages between recvs, your next recv will
-     *   be 32 bytes. Similarly, if there's 200 bytes to receive but you only
-     *   read 149, there will be messages left in your socket's buffer. */
-    /*
-    if ((read_size = recv(sockfd, srv_reply, PKT_SIZE, 0)) < 0) {
-        perror("recv failed");
-    } else {
-        printf("Received packet: ");
-        print_packet((packet_t *)&srv_reply);
-    }
-    */
 }
